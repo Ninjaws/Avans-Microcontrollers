29c5417db9401455f41e74045862cc71f4c15795
Unknown <ninjaws@protonmail.com>
2019-02-02 12:09:38 +0100

    [Added] Base for exercise B7a

---
+++ Microcontrollers/Week1-B7a/main.c
@@ -0,0 +1,18 @@
+/*
+ * Week1-B7a.c
+ *
+ * Created: 2-2-2019 12:08:08
+ * Author : Gebruiker
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
