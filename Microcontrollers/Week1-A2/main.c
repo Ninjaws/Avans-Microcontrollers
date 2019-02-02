f9c4d1f88bd842d4ba6522d4c96cd209a0881444
Unknown <ninjaws@protonmail.com>
2019-02-02 11:02:23 +0100

    [Added] Base for the exercises

---
+++ Microcontrollers/Week1-A2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Week1-A2.c
+ *
+ * Created: 2-2-2019 10:50:29
+ * Author : Gebruiker
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
