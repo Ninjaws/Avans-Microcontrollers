f4fe28fec85f04938dae708927b3aecf5baf96bf
dkempen <daan.vankempen2@gmail.com>
2019-02-20 12:43:13 +0100

    [Finished] Week 4 B3

--- Microcontrollers/Week4-B3/main.c
+++ Microcontrollers/Week4-B3/main.c
@@ -11,25 +11,31 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#include "lcd.h"
+
 int main(void)
 {
 	DDRF = 0x00;				// set PORTF for input (ADC)
 	DDRA = 0xFF;				// set PORTA for output
 	DDRB = 0xFF;				// set PORTB for output
 	adcInit();					// initialize ADC
+	lcd_init();
 
 	while (1)
 	{
-		//PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
-		PORTA = ADCH;
-		wait(100);				// every 100 ms (busy waiting)
+	    PORTA = ADCH;
+	    char snum[3];
+		lcd_clear();
+		lcd_set_cursor(0);
+		lcd_write_string(itoa(ADCH << 1, snum, 10));
+		wait(1000);				// every 100 ms (busy waiting)
 	}
 }
 
 void adcInit( void )
 {
-	ADMUX  = 0b01100011;			// AREF=VCC, result left adjusted, channel3 at pin PF3
-	ADCSRA = 0b11000110;		// ADC-enable, no interrupt, start, not free running, division by 64
+	ADMUX  = 0b11100001;		// AREF=VCC, result left adjusted, channel3 at pin PF3, reference voltage is 2,56V
+	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
 }
 
 void wait( int ms )
